e5a564eb66512c3bc33144f73890c248b153190b
Eddy.Kim <sieunkr@gmail.com>
2020-02-02 18:51:09 +0900

    spring cloud task sample

---
+++ parser/src/main/java/eddy/io/news/parser/configuration/TaskConfiguration.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *          https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package eddy.io.news.parser.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@EnableTask
+public class TaskConfiguration {
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> log.info("step01 : news parser");
+	}
+}
---
+++ parser/src/test/java/eddy/io/news/parser/ParserApplicationTests.java
@@ -0,0 +1,13 @@
+package eddy.io.news.parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
